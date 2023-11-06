e92c933653143fa2cea49fa095d705947342e31a
Asadov Elbotir <elbotir2008@gmail.com>
2023-11-07 00:40:17 +0500

    header and footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,80 @@
-import React from "react";
-
+import "./footer.scss";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-1 flex-class">
+          <h1>Subscribe to our Newsletter</h1>
+          <div className="footer-form flex-class">
+            <input type="text" placeholder="Your Email Address" />
+            <button>Submit</button>
+          </div>
+        </div>
+        <div className="footer-2 grid-class">
+          <div className="grid-1">
+            <h1>Contact Us</h1>
+            <ul>
+              <li>
+                <b>Email</b>
+                <p>needhelp@Organia.com</p>
+              </li>
+              <li>
+                <b>Phone</b>
+                <p>666 888 888</p>
+              </li>
+              <li>
+                <b>Address</b>
+                <p>88 road, borklyn street, USA</p>
+              </li>
+            </ul>
+          </div>
+          <div className="grid-2">
+            <img
+              src="./HomePageImages/Logo.svg"
+              className="footer-img1"
+              alt="Eror"
+            />
+            <p>
+              Simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum simply dummy text of the printing
+              <div className="flex">
+                <img src="./HomePageImages/instagramm.svg" alt="Eror" />
+                <img src="./HomePageImages/facebook.svg" alt="Eror" />
+                <img src="./HomePageImages/twitter.svg" alt="Eror" />
+                <img src="./HomePageImages/pinterest.svg" alt="Eror" />
+              </div>
+            </p>
+          </div>
+          <div className="grid-3">
+            <h1>Utility Pages</h1>
+            <ul>
+              <li>
+                <p>Style Guide</p>
+              </li>
+              <li>
+                <p>404 Not Found</p>
+              </li>
+              <li>
+                <p>Password Protected</p>
+              </li>
+              <li>
+                <p>Licences</p>
+              </li>
+              <li>
+                <p>Changelog</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-3">
+          <p>
+            Copyright © <b> Organick </b> | Designed by <b> VictorFlow </b>
+            Templates - Powered by <b> Webflow</b>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,4 +1,3 @@
-import { korzinka, logo, search } from "@/public/homeImages";
 import "./header.scss";
 const Header = () => {
   return (
@@ -6,8 +5,7 @@ const Header = () => {
       <div className="container">
         <nav className="flex-class">
           <div className="nav-logo flex-class">
-            <img src={logo} alt="Eror" />
-            <h1>Organick</h1>
+            <img src="./HomePageImages/Logo.svg" alt="Eror" />
           </div>
           <div className="nav-links">
             <ul className="flex-class">
@@ -32,8 +30,8 @@ const Header = () => {
             </ul>
           </div>
           <div className="nav-btns flex-class">
-            <img src={search} alt="Eror" />
-            <img src={korzinka} alt="Eror" />
+            <img src="./HomePageImages/Search Icon.svg" alt="Eror" />
+            <img src="./HomePageImages/korzinka.svg" alt="Eror" />
             <h4>Cart (0)</h4>
           </div>
         </nav>
